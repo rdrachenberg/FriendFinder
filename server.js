3720202f191d670e951f5f3eb43dbaa7b3a9ea9b
Ryan Drachenberg <ryan@Ryans-MacBook-Pro.local>
2018-02-16 01:02:59 -0500

    dependancies added

---
+++ server.js
@@ -0,0 +1,7 @@
+// requrie dependancies 
+var express = requrie("express"); 
+var bodyParser = require("body-parser");
+var path = requrie("path");
+// dependancies installed 
+// .gitignore file added with dependancies ignored 
+
